899e944515515958493596ac4266271aa5865ed2
kent0304 <tanaken0304.0123@gmail.com>
2021-01-09 01:26:47 +0900

    Add new model

---
+++ model.py
@@ -0,0 +1,55 @@
+# 正誤判定モデル
+# 入力は画像(2048)、語句(300)、正答文(300)、誤答文(300)
+# 出力はラベル(0, 1)
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+import tqdm
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader, TensorDataset
+from torchvision import models
+from PIL import Image
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.image_fc1 = nn.Linear(2048, 1536)
+        self.image_fc2 = nn.Linear(1536, 1024)
+        self.image_fc3 = nn.Linear(1024, 512)
+
+        self.key_fc1 = nn.Linear(300, 372)
+        self.key_fc2 = nn.Linear(372, 446)
+        self.key_fc3 = nn.Linear(446, 512)
+
+        self.ans_fc1 = nn.Linear(300, 372)
+        self.ans_fc2 = nn.Linear(372, 446)
+        self.ans_fc3 = nn.Linear(446, 512)
+
+        self.total_fc1 = nn.Linear(1536, 768)
+        self.total_fc2 = nn.Linear(768, 384)
+        self.total_fc3 = nn.Linear(384, 2)
+
+
+
+    def forward(self, image, key, ans):
+        image = F.relu(self.image_fc1(image))
+        image = F.relu(self.image_fc2(image))
+        image = self.image_fc3(image)
+
+        key = F.relu(self.key_fc1(key))
+        key = F.relu(self.key_fc2(key))
+        key = self.key_fc3(key)
+
+        ans = F.relu(self.ans_fc1(ans))
+        ans = F.relu(self.ans_fc2(ans))
+        ans = self.ans_fc3(ans)
+
+        input_feature = torch.cat(image, key, ans, axis=1)
+
+        output = F.relu(self.total_fc1(input_feature))
+        output = F.relu(self.total_fc2(output))
+        output = self.total_fc3(output)
+
+        return output
