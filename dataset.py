67d0f07a7a5acd1dd7d627449de63f054af2b259
kent0304 <tanaken0304.0123@gmail.com>
2021-02-17 22:46:26 +0900

    using sbert

--- dataset.py
+++ dataset.py
@@ -1,20 +1,26 @@
+import os
 import pickle
 import torch 
 from torch.utils.data import Dataset, DataLoader
+from sentence_transformers import SentenceTransformer
 
 class MyDataset(Dataset):
-    def __init__(self, imagedata, keydata, ansdata, labeldata):
-        self.imagevec = imagedata
-        self.keyvec = keydata
-        self.ansvec = ansdata
-        self.label = labeldata
-        self.data_num = len(self.imagevec)
+    def __init__(self, dirname, p):
+        self.filename = os.path.join('/mnt/LSTA5/data/tanaka/lang-learn/coco/txtfile/', dirname, p)
+        self.data_num = len(self.filename)
+        self.sbert_model = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
         
     def __len__(self):
         return self.data_num
 
     def __getitem__(self, idx):
-        image = self.imagevec[idx]
+        with open(self.filename) as f:
+            
+
+        
+        image = self.imagevec[self.imagedata[idx]]
+
+        self.sbert_model.encode(text)
         key = self.keyvec[idx]
         ans = self.ansvec[idx]
         label = self.label[idx]
