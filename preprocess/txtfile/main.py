2d4acc01730ed6874ab0f41a3092e003fb5dac40
kent0304 <tanaken0304.0123@gmail.com>
2021-02-21 10:29:26 +0900

    Delete unneeds

--- preprocess/txtfile/main.py
+++ preprocess/txtfile/main.py
@@ -1,4 +1,6 @@
 import json 
+import pickle
+import itertools
 from tqdm import tqdm
 from nltk import stem
 lemmatizer = stem.WordNetLemmatizer()
@@ -7,7 +9,8 @@ lemmatizer = stem.WordNetLemmatizer()
 def each_write_txt(json_obj, ver):
     # 出力するファイルをstringで管理
     output = ''
-    image_idx = 0
+    image_idx = -1
+    pre_imageid = ''
     for image_set in tqdm(json_obj.items(), total=len(json_obj)):
         imageid = image_set[0]
         meta = image_set[1]
@@ -16,56 +19,72 @@ def each_write_txt(json_obj, ver):
             if k != []:
                 # 原型に変換
                 k = ' '.join([lemmatizer.lemmatize(elm, pos='v') for elm in k])
+                if pre_imageid != imageid:
+                    image_idx += 1
                 cap = meta['captions'][i].strip().replace('\n', ' ')
                 row = imageid + '\t' + k + '\t' + cap + '\t' + str(1) + '\t' + str(image_idx) + '\n'
+                pre_imageid = imageid
                 output += row
                 for n_cap in meta[ver+'_noise_captions'][i]:
                     row = imageid + '\t' + k + '\t' + n_cap.strip().replace('\n', ' ')+ '\t' + str(0) + '\t' + str(image_idx) + '\n'
                     output += row
-        image_idx += 1
+
     return output
 
 def write_txt(json_obj, ver):
     # 出力するファイルをstringで管理
     output = ''
-    image_idx = 0
+    image_idx = -1
+    pre_imageid = ''
     for image_set in tqdm(json_obj.items(), total=len(json_obj)):
         imageid = image_set[0]
         meta = image_set[1]
         for i, k in enumerate(meta['key']):
+            
             # 語句が存在するかどうか
             if k != []:
                 # 原型に変換
                 k = ' '.join([lemmatizer.lemmatize(elm, pos='v') for elm in k])
-                cap = meta['captions'][i].strip().replace('\n', ' ')
+                if pre_imageid != imageid:
+                    image_idx += 1
+                cap = meta['captions'][i].strip().replace('\n', ' ').replace('\t', '')
                 row = imageid + '\t' + k + '\t' + cap + '\t' + str(1) + '\t' + str(image_idx) + '\n'
+                pre_imageid = imageid
                 output += row
-                n_cap = meta[ver+'_noise_captions'][i].strip().replace('\n', ' ')
+                n_cap = meta[ver+'_noise_captions'][i].strip().replace('\n', ' ').replace('\t', '').replace('"', '')
                 row = imageid + '\t' + k + '\t' + n_cap + '\t' + str(0) + '\t' + str(image_idx) + '\n'
+                pre_imageid = imageid
                 output += row
-        image_idx += 1
+
     return output
 
-def random_write_txt(json_obj):
-    # 出力するファイルをstringで管理
-    output = ''
-    image_idx = 0
-    for image_set in tqdm(json_obj.items(), total=len(json_obj)):
-        imageid = image_set[0]
-        meta = image_set[1]
-        for i, k in enumerate(meta['key']):
-            # 語句が存在するかどうか
-            if k != []:
-                # 原型に変換
-                k = ' '.join([lemmatizer.lemmatize(elm, pos='v') for elm in k])
-                cap = meta['captions'][i].strip().replace('\n', ' ')
-                row = imageid + '\t' + k + '\t' + cap + '\t' + str(1) + '\t' + str(image_idx) + '\n'
-                output += row
-                n_cap = meta['noise_captions'][i].strip().replace('\n', ' ')
-                row = imageid + '\t' + k + '\t' + n_cap + '\t' + str(0) + '\t' + str(image_idx) + '\n'
-                output += row
-        image_idx += 1
-    return output
+# def random_write_txt(json_obj):
+#     # 出力するファイルをstringで管理
+#     output = ''
+#     image_idx = -1
+#     imageid_set = []
+#     pre_imageid = ''
+#     for image_set in tqdm(json_obj.items(), total=len(json_obj)):
+#         imageid = image_set[0]
+#         meta = image_set[1]
+#         for i, k in enumerate(meta['key']):
+#             # 語句が存在するかどうか
+#             if k != []:
+#                 # 原型に変換
+#                 k = ' '.join([lemmatizer.lemmatize(elm, pos='v') for elm in k])
+#                 if pre_imageid != imageid:
+#                     image_idx += 1
+#                 cap = meta['captions'][i].strip().replace('\n', ' ')
+#                 row = imageid + '\t' + k + '\t' + cap + '\t' + str(1) + '\t' + str(image_idx) + '\n'
+#                 pre_imageid = imageid
+#                 output += row
+#                 n_cap = meta['noise_captions'][i].strip().replace('\n', ' ')
+#                 row = imageid + '\t' + k + '\t' + n_cap + '\t' + str(0) + '\t' + str(image_idx) + '\n'
+#                 output += row
+#         imageid_set.append(imageid)
+#     print(image_idx)
+#     print(len(set(imageid_set)))
+#     return output
 
 def main():
     # open file
@@ -76,13 +95,13 @@ def main():
     train_img2info = json.load(train_img2info)
     val_img2info = json.load(val_img2info)
 
-    ver_list = ['berteach_wn025', 'berteach_wn05', 'berteach_wn075', 'bert_wn025', 'bert_wn05', 'bert_wn075', 'random']
+    ver_list = ['berteach_wn025', 'berteach_wn05', 'berteach_wn075', 'bert_wn025', 'bert_wn05', 'bert_wn075']
 
     for ver in ver_list:
         if 'each' in ver:
             output = each_write_txt(train_img2info, ver)
-        elif ver == 'random':
-            output = random_write_txt(train_img2info)
+        # elif ver == 'random':
+        #     output = random_write_txt(train_img2info)
         else:
             output = write_txt(train_img2info, ver)
         with open('/mnt/LSTA5/data/tanaka/lang-learn/coco/txtfile/{}/train/output.txt'.format(ver), 'w') as f:
@@ -90,8 +109,8 @@ def main():
 
         if 'each' in ver:
             output = each_write_txt(val_img2info, ver)
-        elif ver == 'random':
-            output = random_write_txt(val_img2info)
+        # elif ver == 'random':
+        #     output = random_write_txt(val_img2info)
         else:
             output = write_txt(val_img2info, ver)
         with open('/mnt/LSTA5/data/tanaka/lang-learn/coco/txtfile/{}/valid/output.txt'.format(ver), 'w') as f:
